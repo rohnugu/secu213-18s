2fd6762c06b05fef5105441e35d0776d5b92ddf2
jojejin <37167652+jojejin@users.noreply.github.com>
2018-04-05 10:28:45 +0900

    Create client_2017270712.c

---
+++ 01TCPHelloWorld/client_2017270712.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void main() {
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(sockfd == -1) {
+		printf("socket error\n");
+		exit(1);
+	}
+
+	struct sockaddr_in serveraddr;
+
+	memset(&serveraddr, 0, sizeof(struct sockaddr_in));
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(8080);
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int serverfd = connect(sockfd, serveraddr, sizeof(struct sockaddr_in));
+
+	if(serverfd == -1) {
+		printf("connect error\n");
+		exit(1);
+	}
+
+	memset(buf, 0, 1000);
+	strcpy(buf, "World");
+
+	write(connfd, buf, strlen(buf) + 1);
+
+	char buf[1000];
+
+	int sz = read(connfd, buf, 1000);
+
+	buf[sz] = '\0';
+
+	printf("%s", buf);
+
+	close(connfd);
+	close(serveraddr);
+}
