87c7b71c66cc85c05f25481309660b4271025f09
Heejun Roh <rohnugu@gmail.com>
2018-04-11 00:32:21 +0900

    Server for Vagrant

---
+++ 01TCPHelloWorld/server_vagrant.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void main() {
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(sockfd == -1) {
+		printf("socket error\n");
+		exit(1);
+	}
+
+	struct sockaddr_in addr;
+
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int err = bind(sockfd, (const struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+	if(err == -1) {
+		printf("bind error\n");
+		exit(-1);
+	}
+
+	err = listen(sockfd, 5);
+	if(err == -1) {
+		printf("listen error\n");
+	}
+
+	struct sockaddr clientaddr;
+	int szclientaddr;
+
+	for(;;) {
+		int connfd = accept(sockfd, &clientaddr, &szclientaddr);
+		if(fork() == 0) {
+			close(sockfd);
+
+			char buf[1000];
+
+			int sz = read(connfd, buf, 1000);
+
+			buf[sz] = '\0';
+
+			printf("%s", buf);
+
+			memset(buf, 0, 1000);
+			strcpy(buf, "World");
+
+			write(connfd, buf, strlen(buf) + 1);
+			exit(0);
+		}
+		close(connfd);
+	}
+}
